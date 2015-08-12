e2f47b977beae4dac45c5714707467a69b87dcbd
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2015-08-12 09:17:08 -0500

    add page styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,3 +16,62 @@
 
   }
 });
+  requirejs(
+  ["jquery","lodash","firebase","hbs","bootstrap"],
+  function($, _,firebase,Handlebars,bootstrap){
+
+
+
+    $('.collapse').collapse();
+
+
+
+  ///// mouseover, mouseout, click effects ///////////////////////////////////
+
+$('.sidebar-MyMovies').on('mouseover', function(){
+  $(this).addClass('red');
+});
+
+$('.sidebar-MyMovies').on('mouseout', function(){
+  $(this).removeClass('red');
+});
+
+$('.sidebar-MyMovies').on('click', function(){
+  $(this).toggleClass('red2');
+});
+
+
+$('.sidebar-Shop').on('mouseover', function(){
+  $(this).addClass('blue');
+});
+
+$('.sidebar-Shop').on('mouseout', function(){
+  $(this).removeClass('blue');
+});
+
+$('.sidebar-Shop').on('click', function(){
+  $(this).toggleClass('blue2');
+});
+
+
+$('h2').on('mouseover', function(){
+  $(this).addClass('h2-weight');
+});
+$('h2').on('mouseout', function(){
+  $(this).removeClass('h2-weight');
+});
+
+
+$('.movie-info').on('mouseover', function(){
+  $(this).addClass('shadow');
+});
+$('.movie-info').on('mouseout', function(){
+  $(this).removeClass('shadow');
+});
+
+
+// $('.movie-info').on('click', function(){
+//   $(this).toggleClass('shadow-click');
+// });
+
+});
