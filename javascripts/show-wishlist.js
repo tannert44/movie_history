800bc16d219cf4157f41d728d2ecd6da398fe04c
tannert44 <tannert8155@gmail.com>
2015-08-13 14:58:28 -0500

    Console.log search Array complete

--- javascripts/show-wishlist.js
+++ javascripts/show-wishlist.js
@@ -6,7 +6,14 @@ define(function(){
         url:"http://www.omdbapi.com/?s=" + userInput,
         method: "GET"
       }).done(function(data){
-        
+        console.log("done fired");
+        console.log(data);
+        console.log(data.search);
+        $.each(data.Search, function(index, value){
+          console.log(value);
+          value.poster = "http://img.omdbapi.com/?i=" + value.imdbID + "&apikey=8513e0a1";
+          console.log(value);
+        });
       });
     };
 });
