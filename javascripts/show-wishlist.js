e42ef01a3818aa40f88eb2cc7a0b9441a2856779
tannert44 <tannert8155@gmail.com>
2015-08-13 15:31:18 -0500

    populating model successfully

--- javascripts/show-wishlist.js
+++ javascripts/show-wishlist.js
@@ -8,12 +8,16 @@ define(function(){
       }).done(function(data){
         console.log("done fired");
         console.log(data);
-        console.log(data.search);
         $.each(data.Search, function(index, value){
           console.log(value);
           value.poster = "http://img.omdbapi.com/?i=" + value.imdbID + "&apikey=8513e0a1";
           console.log(value);
+          
         });
+        console.log(data.Search);
+        require(['hbs!../templates/model'], function(moviesTemplate){
+            $('.modal-body').html(moviesTemplate(data.Search));
+          });
       });
     };
 });
