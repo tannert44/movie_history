acf71e043642bf7a928caa7d3fdf48d336c703a4
Lewis Wade <lewis@mrwade.net>
2015-08-14 13:18:15 -0500

    Almost corrected watched/wishlist

--- javascripts/populate-wishlist.js
+++ javascripts/populate-wishlist.js
@@ -13,8 +13,8 @@ define(function(){
 
         data.poster = "http://img.omdbapi.com/?i=" + data.imdbID + "&apikey=8513e0a1";
         data.rating = 0;
-        data.wishlist = true;
-        data.watched=false;
+        data.watched = false;
+        data.wishlist = "wishlist";
         console.log("done");
         $.ajax({
           url: "https://movie-history-redo.firebaseio.com/movies.json",
