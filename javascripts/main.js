3b7943c8c057530e33af8860ff9c34556a488005
tannert44 <tannert8155@gmail.com>
2015-08-13 20:00:19 -0500

    Disappeared the Modal and Populated wishlist.

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,8 +16,8 @@ requirejs.config({
   }
 });
 
-requirejs(["jquery", "lodash", "hbs", "bootstrap", "firebase", "show-wishlist", "removeMovie"], 
-  function($, _, Handlebars, bootstrap, _firebase, showWishlist, removeMovie) {
+requirejs(["jquery", "lodash", "hbs", "bootstrap", "firebase", "show-wishlist", "populate-wishlist","removeMovie"], 
+  function($, _, Handlebars, bootstrap, _firebase, showWishlist, popWishlist, removeMovie) {
   
   var myFirebaseRef = new Firebase("https://movie-history-redo.firebaseio.com/movies");
   myFirebaseRef.on("value", function(snapshot) {
@@ -47,31 +47,13 @@ requirejs(["jquery", "lodash", "hbs", "bootstrap", "firebase", "show-wishlist", 
   });
   $(document).on("click", ".add-button", function(){
     showWishlist();
-    $(document).on("click", ".add-movie", function(){
-      console.log("button clicked");
-      var $titleModel = $(this).parent().attr("class");
-      console.log($titleModel);
-      $.ajax({
-        url:"http://www.omdbapi.com/?t=" + $titleModel,
-        method: "GET"
-      }).done(function(data){
-        console.log("done fired");
-        console.log(data);
-        
-        data.poster = "http://img.omdbapi.com/?i=" + data.imdbID + "&apikey=8513e0a1";
-        data.rating = 0;
-        data.watched=false;
-        console.log("done");
-        $.ajax({
-          url: "https://movie-history-redo.firebaseio.com/movies.json",
-          method: "POST",
-          data: JSON.stringify(data)
-        }).done(function(){
-          console.log("loaded to firebase");
-        });
-      });
+    popWishlist();
+    $(document).on("click", ".done-button", function(){
+      
+      $('#myModal').modal('hide');
     });
   });
+  
   
   
   
